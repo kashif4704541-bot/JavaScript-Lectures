1441cdfc2e10d2366d9710a525c628920dc21963
Shaheer.kas <kashif4704541@cloud.neduet.edu.pk>
2025-08-07 13:00:36 +0500

    Add files via upload

---
+++ 2_Operators in JavaScript/script.js
@@ -0,0 +1,29 @@
+// Operators are used to perform some operation
+
+// 1. Arithmetic Operator (+,-,*,/,%,**,++,--)
+
+let a = 24
+let b = 95
+console.log(a+b, a-b, a*b, b/a, b%a, b**a)
+
+// 2. Assignment Opearator(= , += , -= , *= , /= , %= , **=)
+
+let ass = 23
+ass += 10;
+console.log(ass)
+
+// 3. Comparision Operator (== , === , != , !== , > , < , <= , >= )
+
+console.log('5' == 5)     
+console.log('5' === 5)  
+
+// 4. Logical Operator ( && , || , ! )
+
+let x = true;
+let y = false;
+console.log(x && y);
+console.log(x || y);
+
+let loggedIn = false;
+console.log(!loggedIn)
+
